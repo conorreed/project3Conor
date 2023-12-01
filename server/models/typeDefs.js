7e76d60107fba90a3ce0c5867f1398fd81d98512
MWandall <144394104+MWandall@users.noreply.github.com>
2023-12-01 14:36:30 -0600

    starting on type defs and schemas

---
+++ server/models/typeDefs.js
@@ -0,0 +1,45 @@
+const typeDefs = `
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    savedChars: [Character]!
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Character {
+    _id: ID!
+    name: String
+    // level: Int
+    health: Int
+    attack: Int
+    // defense: Int
+  }
+
+  input inputCharacter {
+    name: String
+    // level: Int
+    health: Int
+    attack: Int
+    // defense: Int
+  }
+
+  type Query {
+    users: [User]
+    singleUser(username: String!): User
+    characters: [Character]
+    singleCharacter(_id: ID!): Character
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveCharacter(username: String!, newCharacter: inputCharacter!): Character
+  }
+`;
+
+module.exports = typeDefs;
