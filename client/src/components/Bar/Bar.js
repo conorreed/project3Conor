e8f224a3c962f719b6a938547c08834d905a9d3d
Conor <conor.reed@gmail.com>
2023-12-05 12:52:15 -0600

    added in Bar and BattleAnnouncer components

--- client/src/components/Bar/Bar.js
+++ client/src/components/Bar/Bar.js
@@ -1,1 +1,14 @@
 // where we keep the "bar" component. Will have a max value, the actual value, and we can use it for xp, hp and notoriety bar
+import styles from './styles.module.css';
+
+export const Bar = ({ value, maxValue, label }) => (
+  <div className={styles.main}>
+    <div className={styles.label}>{label}</div>
+    <div className={styles.max}>
+      <div
+        className={styles.value}
+        style={{ width: `${(value / maxValue) * 100}%` }}
+      ></div>
+    </div>
+  </div>
+);
--- client/src/components/BattleAnnouncer/BattleAnnouncer.js
+++ client/src/components/BattleAnnouncer/BattleAnnouncer.js
@@ -1,1 +1,13 @@
 // battle announcer will use typed messages to type out the actions that happen during the battle
+import { useTypedMessage } from 'hooks';
+import styles from './styles.module.css';
+
+export const BattleAnnouncer = ({ message }) => {
+  const typedMessage = useTypedMessage(message);
+
+  return (
+    <div className={styles.main}>
+      <div className={styles.message}>{typedMessage}</div>
+    </div>
+  );
+};
