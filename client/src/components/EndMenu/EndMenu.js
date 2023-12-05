147c6280a08726f99b424e3f1df25d32c4212f39
Conor <conor.reed@gmail.com>
2023-12-05 12:55:16 -0600

    added EndMenu Component

--- client/src/components/EndMenu/EndMenu.js
+++ client/src/components/EndMenu/EndMenu.js
@@ -1,1 +1,13 @@
 // this is where the "YOU WIN" screen will pop up. Could add in rewards to this screen depending on what we wanna do
+import styles from './styles.module.css';
+
+export const EndMenu = ({ winner, onStartClick }) => {
+  return (
+    <div className={styles.main}>
+      <h1>{winner.name} has won!</h1>
+      <button className={styles.startButton} onClick={onStartClick}>
+        Play Again
+      </button>
+    </div>
+  );
+};
