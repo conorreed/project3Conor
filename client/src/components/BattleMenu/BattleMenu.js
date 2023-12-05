5303eb74cfa81740cf7d407bb8177bccec83e94b
Conor <conor.reed@gmail.com>
2023-12-05 12:54:05 -0600

    added in BattleMenu component which holds the buttons for Attack, Magic, and Heal

--- client/src/components/BattleMenu/BattleMenu.js
+++ client/src/components/BattleMenu/BattleMenu.js
@@ -1,1 +1,16 @@
 // this is where the Attack, Magic attack, and Heal buttons will live
+import styles from './styles.module.css';
+
+export const BattleMenu = ({ onAttack, onMagic, onHeal }) => (
+  <div className={styles.main}>
+    <div onClick={onAttack} className={styles.option}>
+      Attack
+    </div>
+    <div onClick={onMagic} className={styles.option}>
+      Magic
+    </div>
+    <div onClick={onHeal} className={styles.option}>
+      Heal
+    </div>
+  </div>
+);
