7c5f2dc7d102b322f95a708c59a1eb6a2dbf89f5
MWandall <144394104+MWandall@users.noreply.github.com>
2023-12-06 09:59:56 -0600

    animation fix

--- client/src/pages/Battle.jsx
+++ client/src/pages/Battle.jsx
@@ -7,44 +7,55 @@ import { Link } from "react-router-dom";
 
 const Battle = () => {
   const [effect, setEffect] = useState(false);
+  // const [hide, setHide] = useState(false)
+
+
+
+
   return (
-    <div className=" battleBackground h-screen w-screen flex text-blue-100 bg-gray-800">
-      {/* <h1 className="p-5">Didnt get to this yet</h1> */}
-
-      <div className="  flex flex-col justify-end text-center  w-screen">
-        <div className=" charContainer  flex flex-row justify-between mb-5 ">
-          {/* margin for charContainer is in index css. couldnt get inline to work right */}
-
-          
+    <div className="battleBackground h-screen w-screen flex text-blue-100 bg-gray-800">
+      <div className="flex flex-col justify-end text-center w-screen">
+        <div className="charContainer flex flex-row justify-between mb-5">
           <div
-            id="animatedImage"
-            className={`${effect && "animate-wiggle"}  p-4 flex flex-col`}
+            className="p-4 flex flex-col"
             onClick={() => {
-              setEffect(true);
-            }}
+              setEffect(true)}}
             onAnimationEnd={() => setEffect(false)}
-            // className="flex flex-row "
+          
           >
             <p className="py-1.5 text-fuchsia-400">hp and better looking card here</p>
-            <div className="flex flex-row  justify-between rounded-md h-[70px] bg-slate-400 ">
-              
+            <div className={`flex flex-row justify-between rounded-md h-[70px] bg-slate-400 `}>
               <button className="bg-red-500 text-white px-4 py-2 rounded">
                 Attack
               </button>
-              <Link to="/Lair"><button className="bg-red-500 text-white px-4 py-2 rounded">
-                Run
-              </button></Link>
+              <Link to="/Lair">
+                <button className="bg-red-500 text-white px-4 py-2 rounded">
+                  Run
+                </button>
+              </Link>
             </div>
 
-            {/* i cant remember how to do it but we can toggle the "hidden" class based off what char the user chooses */}
-            
             <div className="monster flex flex-row justify-end">
-              <img src="/gif/monsters/ghost.gif" alt="ghost" className=" hidden" />
-              <img src="/gif/monsters/skel.gif" alt="skel"  className="hidden"/>
-              <img src="/gif/monsters/frog.gif" alt="frog" className="" />
+              <img
+                src="/gif/monsters/ghost.gif"
+                alt="ghost"
+                className={`hidden ${effect && "animate-wiggle"}`}
+              />
+              <img
+                src="/gif/monsters/skel.gif"
+                alt="skel"
+                className={`hidden ${effect && "animate-wiggle"}`}
+              />
+              <img
+                src="/gif/monsters/frog.gif"
+                alt="frog"
+                className={` ${effect && "animate-wiggle"}`}
+              />
               <img
                 src="/gif/monsters/bat.gif"
-                alt="bat" className="hidden" />
+                alt="bat"
+                className={`hidden ${effect && "animate-wiggle"}`}
+              />
             </div>
           </div>
 
